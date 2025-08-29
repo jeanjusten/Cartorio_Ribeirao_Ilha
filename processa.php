e31ed8766bd62f9897f1383be4e3551871c7ba66
Jean Justen <jean.cjm@hotmail.com>
2025-08-28 23:01:17 -0300

    added wedding section info

--- processa.php
+++ processa.php
@@ -9,7 +9,7 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
 // Form Settings
-$destinatario = "jean.cjm@hotmail.com"; //"cartorioribeirao@cartorioribeirao.com.br"
+$destinatario = "cartorioribeirao@cartorioribeirao.com.br"; //"cartorioribeirao@cartorioribeirao.com.br"
 $assunto = "Nova Inscrição - Casamento Coletivo";
 
 // SMTP Gmail
